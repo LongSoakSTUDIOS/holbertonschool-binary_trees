b4726c0d91eac8abfbf9d66bfa36160228d3f422
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-21 10:12:53 +1000

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -29,6 +29,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		tmp = parent->left;
 		tmp->parent = new_node;
+		parent->left = new_node;
 		new_node->left = tmp;
 	}
 	else
