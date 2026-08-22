84aa073ba2d63a7f73db29b756cd9e35a3027c0a
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-22 14:55:49 +1000

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_uncle - find uncle
+ * @node: pointer to node
+ *
+ * Return: uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+
+    if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
+    {
+        return (NULL);
+    }
+
+    if (node->parent->parent->left == node->parent)
+    {
+        return (node->parent->parent->right);
+    }
+    else
+        return (node->parent->parent->left);
+
+    return (NULL);
+}
